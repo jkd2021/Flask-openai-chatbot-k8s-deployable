66dcb02bf16c6fc63182dda28a52d9ce99a6715c
kangdong <k.jin@tu-braunschweig.de>
2024-08-29 16:54:11 +0200

    chatgpt-3.5 --> 4o

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from openai import OpenAI
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 server = Flask(__name__)
-system_prompt = "You are the personal assistant of Kangdong!"
+system_prompt = "You are the personal assistant of Kangdong Jin!"
 
 
 def send_gpt(prompt):
@@ -28,7 +28,7 @@ def get_request_json():
     if request.method == 'POST':
         if len(request.form['question']) < 1:
             return render_template(
-                'chat3.5.html', question="NULL", res="Question can't be empty!")
+                'chat4o.html', question="NULL", res="Question can't be empty!")
         question = request.form['question']
         print("======================================")
         print("Receive the question:", question)
@@ -36,8 +36,8 @@ def get_request_json():
         print("Q：\n", question)
         print("A：\n", res)
 
-        return render_template('chat3.5.html', question=question, res=str(res))
-    return render_template('chat3.5.html', question=0)
+        return render_template('chat4o.html', question=question, res=str(res))
+    return render_template('chat4o.html', question=0)
 
 if __name__ == '__main__':
     server.run(debug=True, host='0.0.0.0', port=5000)
